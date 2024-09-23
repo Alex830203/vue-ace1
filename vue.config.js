fe3d313b7d4177e1e28a3574028ca405e0fc2fcd
Alex830203 <u0921988551@gmail.com>
2024-09-23 22:53:17 +0800

    解決 Invalid Host header

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',  // 允許外部 IP 訪問
+    port: 8080,       // 根據你實際使用的端口號設置
+    allowedHosts: 'all', // 允許所有主機名，解決 Invalid Host header 問題
+  }
 })
