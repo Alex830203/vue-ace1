6bbf69adf6a14341dd2dccbec80e266a076120cb
Alex830203 <u0921988551@gmail.com>
2024-09-25 17:56:07 +0800

    改https: false

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,10 @@ module.exports = defineConfig({
   devServer: {
     host: '0.0.0.0',  // 允許外部 IP 訪問
     port: 8080,       // 根據你實際使用的端口號設置
-    https: true,      // 開啟 HTTPS 支持
+    https: false,      // 開啟 HTTPS 支持
     allowedHosts: 'all', // 允許所有主機名，解決 Invalid Host header 問題
     client: {
-      webSocketURL: 'wss://alex777.xyz/ws',  // 使用加密的 WebSocket 連接
+      webSocketURL: 'ws://alex777.xyz/ws',
     },
   }
 })
